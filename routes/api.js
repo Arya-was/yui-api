e5533328289ffed1791acdf8c0f4f14e7e346625
Ichinose Honami <74448616+Arya-was@users.noreply.github.com>
2022-02-18 17:44:37 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,20 @@
-const { youtube } = require('../src/downloader')
+const { youtube, tiktok, instagram, instagramStory, twitter, facebook, soundcloud } = require('../src/downloader')
+const { nhread, nhdetail, nhartist, nhtag, nhhome, nhsearch } = require('../src/other')
 const express = require('express');
 const router = express.Router();
 
 router.get('/youtube', youtube);
+router.get('/tiktok', tiktok);
+router.get('/instagram', instagram);
+router.get('/instagramStory', instagramStory);
+router.get('/twitter', twitter);
+router.get('/facebook', facebook);
+router.get('/soundcloud', soundcloud);
+router.get('/nhread/:path', nhread);
+router.get('/nhdetail', nhdetail);
+router.get('/nhartist', nhartist);
+router.get('/nhtag', nhtag);
+router.get('/nhhome', nhhome);
+router.get('/nhsearch', nhsearch);
 
 module.exports = router;
