309939e6cc11b3ad5138bde39ed9a733bf62955f
Ichinose Honami <74448616+Arya-was@users.noreply.github.com>
2022-02-18 17:43:06 +0700

    Update index.js

--- src/downloader/index.js
+++ src/downloader/index.js
@@ -1,4 +1,9 @@
 const { yt } = require('./yt')
+const { tt } = require('./tiktok')
+const { ig, igS } = require('./instagram')
+const { twt } = require('./twitter')
+const { fb } = require('./facebook')
+const { scdl } = require('./soundcloud')
 
 async function youtube(req, res) {
 	const URL = req.query.URL,
@@ -15,6 +20,96 @@ async function youtube(req, res) {
 		});
 }
 
+
+async function tiktok(req, res) {
+	const URL = req.query.URL
+	if (!URL) return res.status(404).json({
+		status: 404,
+		message: 'Input Parameter URL type quality'
+	})
+		const result = await tt(URL)
+		res.status(200).json({
+			status: 200, 
+			result: result
+		});
+}
+
+
+async function instagram(req, res) {
+	const URL = req.query.URL
+	if (!URL) return res.status(404).json({
+		status: 404,
+		message: 'Input Parameter URL'
+	})
+		const result = await ig(URL)
+		res.status(200).json({
+			status: 200, 
+			result: result
+		});
+}
+
+
+async function instagramStory(req, res) {
+	const URL = req.query.URL
+	if (!URL) return res.status(404).json({
+		status: 404,
+		message: 'Input Parameter URL'
+	})
+		const result = await igS(URL)
+		res.status(200).json({
+			status: 200, 
+			result: result
+		});
+}
+
+
+async function twitter(req, res) {
+	const URL = req.query.URL
+	if (!URL) return res.status(404).json({
+		status: 404,
+		message: 'Input Parameter URL'
+	})
+		const result = await twt(URL)
+		res.status(200).json({
+			status: 200, 
+			result: result
+		});
+}
+
+
+async function facebook(req, res) {
+	const URL = req.query.URL
+	if (!URL) return res.status(404).json({
+		status: 404,
+		message: 'Input Parameter URL'
+	})
+		const result = await fb(URL)
+		res.status(200).json({
+			status: 200, 
+			result: result
+		});
+}
+
+
+async function soundcloud(req, res) {
+	const URL = req.query.URL
+	if (!URL) return res.status(404).json({
+		status: 404,
+		message: 'Input Parameter URL'
+	})
+		const result = await scdl(URL)
+		res.status(200).json({
+			status: 200, 
+			result: result
+		});
+}
+
 module.exports = {
-	youtube
+	youtube,
+	tiktok,
+	instagram,
+	instagramStory,
+	twitter,
+	facebook,
+	soundcloud
 }
